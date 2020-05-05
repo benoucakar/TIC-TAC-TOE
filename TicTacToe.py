d2c9a1e263b320f9872df0ee56c8fab9aee24db1
Beno Učakar <beno.ucakar@gmail.com>
2020-05-05 17:37:00 +0200

    Dokončal običajno igro  in začel z ultimate.
    1. Običajna igra se sedaj lahko konča neodločeno.
    2. Funkcija show_field_ultimate() prikaže polje ultimativne igre.
    3. Naredil izgradnjo polja in prvo potezo za Ultimativni Tic Tac Toe.

--- TicTacToe.py
+++ TicTacToe.py
@@ -52,12 +52,28 @@ def show_field_vanila(cell):
     print(f"| {cell.cells[1]} {cell.cells[2]} {cell.cells[3]} |")
     print(" ------- ")
 
+def show_field_ultimate(cells_list):
+    print(" " + "-" * 23 + " ")
+    print(f"| {cells_list[7].cells[7]} {cells_list[7].cells[8]} {cells_list[7].cells[9]} | {cells_list[8].cells[7]} {cells_list[8].cells[8]} {cells_list[8].cells[9]} | {cells_list[9].cells[7]} {cells_list[9].cells[8]} {cells_list[9].cells[9]} |")
+    print(f"| {cells_list[7].cells[4]} {cells_list[7].cells[5]} {cells_list[7].cells[6]} | {cells_list[8].cells[4]} {cells_list[8].cells[5]} {cells_list[8].cells[6]} | {cells_list[9].cells[4]} {cells_list[9].cells[5]} {cells_list[9].cells[6]} |")
+    print(f"| {cells_list[7].cells[1]} {cells_list[7].cells[2]} {cells_list[7].cells[3]} | {cells_list[8].cells[1]} {cells_list[8].cells[2]} {cells_list[8].cells[3]} | {cells_list[9].cells[1]} {cells_list[9].cells[2]} {cells_list[9].cells[3]} |")
+    print(" " + "-" * 23 + " ")
+    print(f"| {cells_list[4].cells[7]} {cells_list[4].cells[8]} {cells_list[4].cells[9]} | {cells_list[5].cells[7]} {cells_list[5].cells[8]} {cells_list[5].cells[9]} | {cells_list[6].cells[7]} {cells_list[6].cells[8]} {cells_list[6].cells[9]} |")
+    print(f"| {cells_list[4].cells[4]} {cells_list[4].cells[5]} {cells_list[4].cells[6]} | {cells_list[5].cells[4]} {cells_list[5].cells[5]} {cells_list[5].cells[6]} | {cells_list[6].cells[4]} {cells_list[6].cells[5]} {cells_list[6].cells[6]} |")
+    print(f"| {cells_list[4].cells[1]} {cells_list[4].cells[2]} {cells_list[4].cells[3]} | {cells_list[5].cells[1]} {cells_list[5].cells[2]} {cells_list[5].cells[3]} | {cells_list[6].cells[1]} {cells_list[6].cells[2]} {cells_list[6].cells[3]} |")
+    print(" " + "-" * 23 + " ")
+    print(f"| {cells_list[1].cells[7]} {cells_list[1].cells[8]} {cells_list[1].cells[9]} | {cells_list[2].cells[7]} {cells_list[2].cells[8]} {cells_list[2].cells[9]} | {cells_list[3].cells[7]} {cells_list[3].cells[8]} {cells_list[3].cells[9]} |")
+    print(f"| {cells_list[1].cells[4]} {cells_list[1].cells[5]} {cells_list[1].cells[6]} | {cells_list[2].cells[4]} {cells_list[2].cells[5]} {cells_list[2].cells[6]} | {cells_list[3].cells[4]} {cells_list[3].cells[5]} {cells_list[3].cells[6]} |")
+    print(f"| {cells_list[1].cells[1]} {cells_list[1].cells[2]} {cells_list[1].cells[3]} | {cells_list[2].cells[1]} {cells_list[2].cells[2]} {cells_list[2].cells[3]} | {cells_list[3].cells[1]} {cells_list[3].cells[2]} {cells_list[3].cells[3]} |")
+    print(" " + "-" * 23 + " ")
+
 
 def start_game_2_vanila():
     game = Cell()
     turn = input_promt_fixed("Bi prvi igralec imel križce ali krožce?", "X/O", "Žal je bil vnos neustrezen.", ["X", "O"])
     win = ""
     num_turns = 0
+    print("Polja so številčena kot številčna tipkovnica.")
     while not game.check_win() and num_turns < 9:
         show_field_vanila(game)
         inp = int(input_promt_fixed(f"Igralec {turn} je na potezi!", "(1 - 9)", "Žal je bil vnos neustrezen.", [str(i) for i in range(1, 10)]))
@@ -81,5 +97,29 @@ def start_game_2_vanila():
     else:
         print("Igra je neodločena.")
 
+def start_game_2_ultimate():
+    celica1 = Cell()
+    celica2 = Cell()
+    celica3 = Cell()
+    celica4 = Cell()
+    celica5 = Cell()
+    celica6 = Cell()
+    celica7 = Cell()
+    celica8 = Cell()
+    celica9 = Cell()
+    game = ["&", celica1, celica2, celica3, celica4, celica5, celica6, celica7, celica8, celica9]
+    turn = "X"
+    print("Polja so številčena kot številčna tipkovnica.")
+    show_field_ultimate(game)
+    inp_cell = int(input_promt_fixed(f"{turn} sme izbrati poljubno celico", "(1 - 9)", "Žal je bil vnos neustrezen.", [str(i) for i in range(1, 10)]))
+    inp_space = int(input_promt_fixed(f"{turn} naj izbere še polje v celici {inp_cell}", "(1 - 9)", "Žal je bil vnos neustrezen.", [str(i) for i in range(1, 10)]))
+    game[inp_cell].cross(inp_space)
+    show_field_ultimate(game)
+    print(inp_cell, inp_space)
+#    while True:
+#        if turn == "X":
+#            None
+#        elif turn == "O":
+#            None
 
-start_game_2_vanila()
+start_game_2_ultimate()
