97af793058faf7a27c7057c9daa5545ae6801026
Beno Učakar <beno.ucakar@gmail.com>
2020-07-08 19:54:51 +0200

    Dodal domačo stran, stan z igrami in stran o projektu.

--- spletni_vmesnik.py
+++ spletni_vmesnik.py
@@ -1,10 +1,24 @@
 import bottle
 from model import *
 
+@bottle.get("/")
+def index():
+    bottle.redirect("/domov/")
 
+@bottle.get("/domov/")
+def domov():
+    return bottle.template("domov.html")
+
+@bottle.get("/igre/")
+def igra():
+    return bottle.template("igre.html")
 
 @bottle.get("/pravila/")
 def pravila():
     return bottle.template("pravila.html")
+
+@bottle.get("/info/")
+def info():
+    return bottle.template("info.html")
 
 bottle.run(debug=True, reloader=True)
