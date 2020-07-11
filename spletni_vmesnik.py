75f4fb6e13617d401ec939dda7fdf43fb2e682c3
Beno Učakar <beno.ucakar@gmail.com>
2020-07-11 10:02:01 +0200

    Spletna naslova /vanila_1/ in /vanila_2/ spremenil v /igre/vanila_1/ in  /igre/vanila_2/

--- spletni_vmesnik.py
+++ spletni_vmesnik.py
@@ -28,12 +28,12 @@ def info():
 # Vanila_2
 vanila_2 = Vanila_2()
 
-@bottle.post("/vanila_2/")
+@bottle.post("/igre/vanila_2/")
 def vanila_2_post():
     if vanila_2.state == "P":
         vanila_2.choose_parameters(bottle.request.forms.getunicode('player_mark'))
         vanila_2.state = "M"
-        bottle.redirect("/vanila_2/")
+        bottle.redirect("/igre/vanila_2/")
     elif vanila_2.state == "M":
         inp_space = int(bottle.request.forms.getunicode('inp_space'))
         vanila_2.check_bad_move()
@@ -42,19 +42,19 @@ def vanila_2_post():
             None
         else:
             vanila_2.state = "E"
-        bottle.redirect("/vanila_2/")
+        bottle.redirect("/igre/vanila_2/")
     elif vanila_2.state == "E":
         vanila_2.reset()
         bottle.redirect("/igre/")
 
-@bottle.get("/vanila_2/")
+@bottle.get("/igre/vanila_2/")
 def vanila_2_get():
     return bottle.template("vanila_2.html", game=vanila_2)
 
 # Vanila_1
 vanila_1 = Vanila_1()
 
-@bottle.post("/vanila_1/")
+@bottle.post("/igre/vanila_1/")
 def vanila_1_post():
     if vanila_1.state == "P":
         player_mark = bottle.request.forms.getunicode('player_mark')
@@ -64,7 +64,7 @@ def vanila_1_post():
         vanila_1.state = "M"
         if not player_turn:
              vanila_1.make_move(next(vanila_1.bot_generator))
-        bottle.redirect("/vanila_1/")
+        bottle.redirect("/igre/vanila_1/")
 
     elif vanila_1.state == "M":
         inp_space = int(bottle.request.forms.getunicode('inp_space'))
@@ -77,13 +77,13 @@ def vanila_1_post():
                     vanila_1.state = "E"
             else:
                 vanila_1.state = "E"                
-        bottle.redirect("/vanila_1/")
+        bottle.redirect("/igre/vanila_1/")
 
     elif vanila_1.state == "E":
         vanila_1.reset()
         bottle.redirect("/igre/")
 
-@bottle.get("/vanila_1/")
+@bottle.get("/igre/vanila_1/")
 def vanila_1_get():
     return bottle.template("vanila_1.html", game=vanila_1)
 
