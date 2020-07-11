6438a416bf4d351f8e5da737d1b74ea24fdde1c7
Beno Učakar <beno.ucakar@gmail.com>
2020-07-11 19:39:50 +0200

    Implementiral ultimativno igro križcev in krožcev za 1 igralca.

--- spletni_vmesnik.py
+++ spletni_vmesnik.py
@@ -57,7 +57,7 @@ vanila_1 = Vanila_1()
 def vanila_1_post():
     if vanila_1.state == "P":
         player_mark = bottle.request.forms.getunicode('player_mark')
-        player_turn = bottle.request.forms.getunicode('player_turn')
+        player_turn = bool(bottle.request.forms.getunicode('player_turn'))
         difficulty = bottle.request.forms.getunicode('difficulty')
         vanila_1.choose_parameters(player_mark, player_turn, difficulty)
         vanila_1.state = "M"
@@ -132,5 +132,96 @@ def ultimate_2_post():
 @bottle.get("/igre/ultimate_2/")
 def ultimate_2_get():
     return bottle.template("ultimate_2.html", game=ultimate_2)
+
+# Ultimate_1
+ultimate_1 = Ultimate_1()
+
+@bottle.post("/igre/ultimate_1/")
+def ultimate_1_post():
+    if ultimate_1.state == "P":
+        player_mark = bottle.request.forms.getunicode('player_mark')
+        player_turn = bool(bottle.request.forms.getunicode('player_turn'))
+        ultimate_1.choose_parameters(player_mark, player_turn)
+        if player_turn:
+            ultimate_1.state = "I"
+        else:
+            inp_cell = ultimate_1.master_cell.random_free()
+            inp_space = ultimate_1.cell_list[inp_cell].random_free()
+            ultimate_1.initial_move(inp_cell, inp_space)
+            ultimate_1.state = "M"
+        bottle.redirect("/igre/ultimate_1/")
+    
+    elif ultimate_1.state == "I":
+        inp_cell = int(bottle.request.forms.getunicode('inp_cell'))
+        inp_space = int(bottle.request.forms.getunicode('inp_space'))
+        ultimate_1.initial_move(inp_cell, inp_space)
+
+        current_cell = ultimate_1.cell_list[ultimate_1.inp_cell]
+        ultimate_1.inp_space = ultimate_1.master_bot.ultimate_incell_move(ultimate_1.cell_list, ultimate_1.inp_cell)
+        ultimate_1.move_in_small_cell(current_cell)
+        ultimate_1.state = "M"
+        bottle.redirect("/igre/ultimate_1/")
+
+    elif ultimate_1.state == "M":
+        
+        #Igralec
+        while ultimate_1.player_turn:
+            current_cell = ultimate_1.cell_list[ultimate_1.inp_cell]
+            if not ultimate_1.move_in_big_cell:
+                inp_space_kand = int(bottle.request.forms.getunicode('inp_space'))
+                if ultimate_1.cell_list[ultimate_1.inp_cell].cells[inp_space_kand] == ".":
+                    ultimate_1.inp_space = inp_space_kand
+                    ultimate_1.move_in_small_cell(current_cell)
+                else:
+                    bottle.redirect("/igre/ultimate_1/")
+            else:
+                inp_cell_kand = int(bottle.request.forms.getunicode('inp_cell'))
+                if ultimate_1.master_cell.cells[inp_cell_kand] == ".":
+                    ultimate_1.inp_cell = inp_cell_kand
+                    ultimate_1.move_in_big_cell = False
+                    # Da se ne pokaže zadnja poteza robota
+                    ultimate_1.last_inp_cell = 0
+                    bottle.redirect("/igre/ultimate_1/")
+                else:
+                    bottle.redirect("/igre/ultimate_1/")
+
+        if ultimate_1.master_cell.cells[ultimate_1.inp_cell] == ".":
+            None
+        else:
+            ultimate_1.move_in_big_cell = True
+
+        if not ultimate_1.master_cell.check_win() and ultimate_1.num_master_turns < 9:
+            #Bot
+            while not ultimate_1.player_turn:
+                current_cell = ultimate_1.cell_list[ultimate_1.inp_cell]
+                if not ultimate_1.move_in_big_cell:
+                    ultimate_1.inp_space = ultimate_1.master_bot.ultimate_incell_move(ultimate_1.cell_list, ultimate_1.inp_cell)
+                    ultimate_1.move_in_small_cell(current_cell)
+                else:
+                    ultimate_1.inp_cell = ultimate_1.master_cell.random_free()
+                    ultimate_1.move_in_big_cell = False
+                
+            if ultimate_1.master_cell.cells[ultimate_1.inp_cell] == ".":
+                None
+            else:
+                ultimate_1.move_in_big_cell = True
+            
+            if not ultimate_1.master_cell.check_win() and ultimate_1.num_master_turns < 9:
+                None
+            else:
+                ultimate_1.state = "E"
+
+        else:
+            ultimate_1.state = "E"
+        bottle.redirect("/igre/ultimate_1/")
+
+
+    elif ultimate_1.state == "E":
+        ultimate_1.reset()
+        bottle.redirect("/igre/")
+
+@bottle.get("/igre/ultimate_1/")
+def ultimate_1_get():
+    return bottle.template("ultimate_1.html", game=ultimate_1)
 
 bottle.run(debug=True, reloader=True)
